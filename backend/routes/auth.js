f656b3c005421fcb3aa2a404de8b781ad9be60d2
akira1442 <nawad.karihila@etu.sorbonne.universite.fr>
2026-05-15 18:55:19 +0200

    debut connexion

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const user = require("../module/Users")
+const user = require("../module/Users");
+const bcrypt = require("bcrypt");
 
 const app = express.Router();
 const jwt = require("jswonwebtoken");
@@ -9,7 +10,8 @@ const JWT_SECRET = "1442";
 app.post("/signin", async (req, res) => {
     
     const {pseudo, mdp} = req.body;
-       
+    
+    // Vérification pseudo
     if (!pseudo){
         const pseudoTmp = pseudo.trim().pseudo.toLowerCase();
         if (pseudoTmp.lenght() < 3){
@@ -20,14 +22,37 @@ app.post("/signin", async (req, res) => {
         }
     }
 
-    const existUser = await user.findOne({username});
+    // Recherche dans la bdd on utilise donc await
+    const existUser = await user.findOne({pseudo});
     
-     try{ 
+    try{ 
         if (existUser){
             return res.status(409).json("Pseudo déjà utilisé");
         }
     }catch (err){
         res.status(500).json("Erreur serveur lors de l'inscription.");
     }
-})
 
+    // Vérification mor de passe
+    if (!mdp){
+        if (mdp.lenght() < 12){
+            return res.status(400).json("Mot de passe top court, 12 caractère minimum avec au moins un chiffre et un caractère spécial");
+        }
+        if (!mdp.test("/^[a-z0-9_]+$/")){
+            return res.status(400).json("Votre mot de passe doit contenir au moins un chiffre et un caractère spécaial");
+        }
+        mdp = bcrypt.hash(mdp);
+    }
+});
+
+// Connexion
+
+app.post("/login", async (req, res) => {
+
+    const {pseudo, mdp} = req.body;
+    
+    if (!pseudo){
+        const pseudoTmp = 
+        const existUser = await user.findOne({username});
+    }
+});
